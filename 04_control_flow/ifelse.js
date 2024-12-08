5ebfa2c13bc4981e6c1caf2279405677862e912a
mastershuvam <130123905+mastershuvam@users.noreply.github.com>
2024-12-08 10:26:43 +0000

    control flow code files

---
+++ 04_control_flow/ifelse.js
@@ -0,0 +1,51 @@
+//if
+
+const isUserLoggerdIn = true
+const temperature = 41
+
+// if(temperature === 40){
+//     console.log("les then 50")
+// }else{
+//     console.log("temperature is geter then 50")
+// }
+
+//console.log("Execute")
+
+//<, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`user power: ${power}`)
+// }
+
+// console.log(`user power:${power}`)
+
+const balance = 1000
+
+//if(balance > 500) console.log("test"),console.log("test2");
+
+// if(balance<500){
+//     console.log("les then 500")
+// }else if(balance<750){
+//     console.log("less then 750")
+// }else if(balance<900){
+//     console.log("less then 900")
+// }else{
+//     console.log("less then 1200")
+// }
+
+
+const userLogedIn = true
+const debtCard = true
+const isGoogleLogedIn = false
+const isEmailLogedIn = true
+
+if(userLogedIn && debtCard){
+    console.log("Allow to buy course")
+}
+
+if(isGoogleLogedIn || isEmailLogedIn){
+    console.log("user logged in")
+}
