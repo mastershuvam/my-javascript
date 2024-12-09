66222e4b5674a1473445623a6e70f4e84a0e4103
mastershuvam <130123905+mastershuvam@users.noreply.github.com>
2024-12-09 12:36:29 +0000

    while loops discussion

---
+++ 05_iterations/while.js
@@ -0,0 +1,20 @@
+let index = 0
+
+// while(index<=10){
+//     console.log(`value of index: ${index}`)
+//     index+=2
+// }
+
+let myArray = ['flash','thor','superman','ironman']
+
+while(index<myArray.length){
+    //console.log(myArray[index])
+    index++;
+}
+
+let score = 11
+
+do{
+    console.log(`score is ${score}`)
+    score++
+}while(score<=10)
