1bf5b41f270ae4b4b892c0b301b2880ff755903a
mastershuvam <130123905+mastershuvam@users.noreply.github.com>
2024-12-14 05:16:28 +0000

    call keyword in javascript

---
+++ 9_classes_and_oop/call.js
@@ -0,0 +1,16 @@
+function SetUserName(username){
+    this.username = username
+    console.log("called")
+}
+
+function createUser(username,email,password){
+    SetUserName.call(this,username)
+
+    this.email = email
+    this.password = password
+}
+
+
+const chai = new createUser("chai","chai@email.com","123")
+
+console.log(chai);
